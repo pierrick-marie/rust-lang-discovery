3af199f2c6438a86c91267b563cb653aaf7960a5
Pierrick <mail@pierrickmarie.info>
2022-04-03 19:22:53 +0200

    Setup all the UI

--- music-player/src/main.rs
+++ music-player/src/main.rs
@@ -37,18 +37,14 @@ use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use gio::glib;
-use gtk::prelude::*;
 
-use gtk::{
-	Button,
-	Inhibit,
-	Label,
-	Window,
-	WindowType,
-	prelude::ButtonExt,
-	prelude::ContainerExt,
-	prelude::LabelExt,
-	prelude::WidgetExt,
+use gtk::{Button, Inhibit, Image, Adjustment, Label, Window, WindowType, ApplicationWindow, FileChooserDialog, FileChooserAction, FileFilter, ResponseType, Scale, IconSize, SeparatorToolItem};
+use gtk::prelude::*;
+use gtk::prelude::{
+	ButtonExt,
+	ContainerExt,
+	LabelExt,
+	WidgetExt,
 };
 use gtk::Orientation::Vertical;
 use relm_derive::Msg;
@@ -60,22 +56,30 @@ mod toolbar;
 use crate::toolbar::MusicToolbar;
 
 struct Model {
-	counter: i32,
+	playlist: Playlist,
+	// counter: i32,
 }
 
 #[derive(Msg)]
 enum Msg {
-	Decrement,
-	Increment,
+	// Decrement,
+	// Increment,
+	Open,
 	Quit,
 }
 
 // Create the structure that holds the widgets used in the view.
 #[derive(Clone)]
 struct Widgets {
-	counter_label: Label,
-	minus_button: Button,
-	plus_button: Button,
+	toolbar: MusicToolbar,
+	cover: Image,
+	adjustment: Adjustment,
+	duration_label: Label,
+	play: Image,
+	pause: Image,
+	// counter_label: Label,
+	// minus_button: Button,
+	// plus_button: Button,
 	window: Window,
 }
 
@@ -94,23 +98,29 @@ impl Update for Win {
 	
 	fn model(_: &Relm<Self>, _: ()) -> Model {
 		Model {
-			counter: 0,
+			playlist: Playlist::new(),
+			// counter: 0,
 		}
 	}
 	
 	fn update(&mut self, event: Msg) {
-		let label = &self.widgets.counter_label;
+		// let label = &self.widgets.counter_label;
 		
 		match event {
-			Msg::Decrement => {
-				self.model.counter -= 1;
-				// Manually update the view.
-				label.set_text(&self.model.counter.to_string());
-			},
-			Msg::Increment => {
-				self.model.counter += 1;
-				label.set_text(&self.model.counter.to_string());
-			},
+			Msg::Open => {
+				for file in Widgets::show_open_dialog(&self.widgets.window) {
+					self.model.playlist.add(&file);
+				}
+			}
+			// Msg::Decrement => {
+			// 	self.model.counter -= 1;
+			// 	// Manually update the view.
+			// 	label.set_text(&self.model.counter.to_string());
+			// },
+			// Msg::Increment => {
+			// 	self.model.counter += 1;
+			// 	label.set_text(&self.model.counter.to_string());
+			// },
 			Msg::Quit => gtk::main_quit(),
 		}
 	}
@@ -127,37 +137,86 @@ impl Widget for Win {
 	
 	fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
 		// Create the view using the normal GTK+ method calls.
-		let vbox = gtk::Box::new(Vertical, 0);
+		let main_container = gtk::Box::new(Vertical, 3);
 		
-		let plus_button = Button::with_label("+");
-		vbox.add(&plus_button);
+		let toolbar = MusicToolbar::new();
 		
-		let counter_label = Label::new(Some("0"));
-		vbox.add(&counter_label);
+		let cover = Image::new();
 		
-		let minus_button = Button::with_label("-");
-		vbox.add(&minus_button);
+		let duration_label = Label::new(Some("0 / 0"));
+		let adjustment = Adjustment::new(0.0, 0.0, 10.0, 0.0, 0.0, 0.0);
+		let scale = gtk::Scale::new(gtk::Orientation::Horizontal, Some(&adjustment));
+		scale.set_draw_value(false);
+		scale.set_hexpand(true);
+		
+		let pause = Image::from_icon_name(Some("gtk-media-pause"), IconSize::LargeToolbar);
+		let play = Image::from_icon_name(Some("gtk-media-play"), IconSize::LargeToolbar);
+		
+		let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 3);
+		hbox.add(&SeparatorToolItem::new());
+		hbox.add(&scale);
+		hbox.add(&duration_label);
+		hbox.add(&SeparatorToolItem::new());
+		
+		main_container.add(&toolbar.container);
+		main_container.add(&cover);
+		main_container.add(&hbox);
+		main_container.add(model.playlist.view());
+		
+		main_container.add(&toolbar.container);
 		
 		let window = Window::new(WindowType::Toplevel);
 		
-		window.add(&vbox);
+		window.add(&main_container);
 		
 		window.show_all();
 		
 		// Send the message Increment when the button is clicked.
-		connect!(relm, plus_button, connect_clicked(_), Msg::Increment);
-		connect!(relm, minus_button, connect_clicked(_), Msg::Decrement);
+		// connect!(relm, plus_button, connect_clicked(_), Msg::Increment);
+		// connect!(relm, minus_button, connect_clicked(_), Msg::Decrement);
+		connect!(relm, toolbar.open_button, connect_clicked(_), Msg::Open);
 		connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
 		
 		Win {
 			model,
 			widgets: Widgets {
-				counter_label,
-				minus_button,
-				plus_button,
+				toolbar,
+				cover,
+				adjustment,
+				duration_label,
+				play,
+				pause,
 				window,
 			},
 		}
+	}
+}
+
+impl Widgets {
+	fn show_open_dialog(parent: &Window) -> Vec<PathBuf> {
+		let mut files = vec![];
+		let dialog = FileChooserDialog::new(Some("Select an MP3 audio file"), Some(parent), FileChooserAction::Open);
+		
+		let mp3_filter = FileFilter::new();
+		mp3_filter.add_mime_type("audio/mp3");
+		mp3_filter.set_name(Some("MP3 audio file"));
+		dialog.add_filter(&mp3_filter);
+		
+		let m3u_filter = FileFilter::new();
+		m3u_filter.add_mime_type("audio/m3u");
+		m3u_filter.set_name(Some("M3U audio playlist"));
+		dialog.add_filter(&m3u_filter);
+		
+		dialog.set_select_multiple(true);
+		dialog.add_button("Cancel", ResponseType::Cancel);
+		dialog.add_button("Accept", ResponseType::Accept);
+		let result = dialog.run();
+		
+		if result == ResponseType::Accept {
+			files = dialog.filenames();
+		}
+		unsafe { dialog.destroy(); }
+		files
 	}
 }
 
