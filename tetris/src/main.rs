60beb739a55bd1c20d085169df006c660d49d2b2
Pierrick <mail@pierrickmarie.info>
2022-04-05 09:53:56 +0200

    Get tetris from tetris branch

---
+++ tetris/src/main.rs
@@ -0,0 +1,16 @@
+mod model;
+mod view;
+
+use model::*;
+use model::tetrimino::*;
+use crate::model::tetrimino;
+use crate::view::*;
+
+fn main() {
+	
+	let mut game: Game = Game::new();
+	let tetrimino: Tetrimino = tetrimino::generate_tetrimino();
+	game.add_tetrimino(tetrimino);
+	let mut view: View = View::new(game);
+	view.run();
+}
