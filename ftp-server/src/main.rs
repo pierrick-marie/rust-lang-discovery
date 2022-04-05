24079353d0a832c945825d790e4fecb9fa25fb67
Pierrick <mail@pierrickmarie.info>
2022-04-05 14:23:25 +0200

    Enable to parse command sent by clients

--- ftp-server/src/main.rs
+++ ftp-server/src/main.rs
@@ -15,8 +15,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with rust-discovery.  If not, see <http://www.gnu.org/licenses/>. */
 
+#[macro_use]
+extern crate num_derive;
+
 mod server;
-use std::env;
 use crate::server::Server;
 
 fn main() {
--- ftp-server/src/server.rs
+++ ftp-server/src/server.rs
@@ -16,17 +16,107 @@ You should have received a copy of the GNU General Public License
 along with rust-discovery.  If not, see <http://www.gnu.org/licenses/>. */
 
 use std::net::{Shutdown, TcpListener, TcpStream};
-use std::io::{BufRead, BufReader, Read, Write};
-use std::ptr::replace;
-use std::str::from_boxed_utf8_unchecked;
+use std::io::{BufRead, BufReader, Write};
 use std::thread;
+use regex::Regex;
+
+extern crate num;
+
+#[derive(FromPrimitive)]
+#[derive(Debug, Clone, Copy)]
+#[repr(u32)]
+#[allow(dead_code)]
+enum FTPCode {
+	RestartMarkerReply = 110,
+	ServiceReadInXXXMinutes = 120,
+	DataConnectionAlreadyOpen = 125,
+	FileStatusOk = 150,
+	Ok = 200,
+	CommandNotImplementedSuperfluousAtThisSite = 202,
+	SystemStatus = 211,
+	DirectoryStatus = 212,
+	FileStatus = 213,
+	HelpMessage = 214,
+	SystemType = 215,
+	ServiceReadyForNewUser = 220,
+	ServiceClosingControlConnection = 221,
+	DataConnectionOpen = 225,
+	ClosingDataConnection = 226,
+	EnteringPassiveMode = 227,
+	UserLoggedIn = 230,
+	RequestedFileActionOkay = 250,
+	PATHNAMECreated = 257,
+	UserNameOkayNeedPassword = 331,
+	NeedAccountForLogin = 332,
+	RequestedFileActionPendingFurtherInformation = 350,
+	ServiceNotAvailable = 421,
+	CantOpenDataConnection = 425,
+	ConnectionClosed = 426,
+	FileBusy = 450,
+	LocalErrorInProcessing = 451,
+	InsufficientStorageSpace = 452,
+	UnknownCommand = 500,
+	InvalidParameterOrArgument = 501,
+	CommandNotImplemented = 502,
+	BadSequenceOfCommands = 503,
+	CommandNotImplementedForThatParameter = 504,
+	NotLoggedIn = 530,
+	NeedAccountForStoringFiles = 532,
+	FileNotFound = 550,
+	PageTypeUnknown = 551,
+	ExceededStorageAllocation = 552,
+	FileNameNotAllowed = 553,
+}
 
 pub struct Server {
 	listener: TcpListener,
 }
 
-impl Server {
+struct Command {
+	code: i32,
+	args: String,
+}
+
+impl Command {
+
+	fn from_string(command: &str) -> Self {
+		let re = Regex::new(r"(\d{3}) (.+)").unwrap();
+		let mut code = -1; // Error code
+		let mut args = "";
+		if let Some(cap) = re.captures(command) {
+			// there are two patterns in the regex + the text itself
+			if 3 == cap.len() {
+				code = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
+				args = cap.get(2).unwrap().as_str();
+			} else {
+				println!("Impossible to read the code");
+			}
+		} else {
+			println!("Impossible to read the command");
+		}
+		Command {
+			code,
+			args: args.to_string(),
+		}
+	}
 	
+	fn exec(&self) {
+		let element = num::FromPrimitive::from_i32(self.code);
+		match element {
+			Some(FTPCode::Ok) => {
+				println!("OK command with args: {}", self.args)
+			},
+			Some(FTPCode::UnknownCommand) => {
+				println!("Unknown command with args: {}", self.args)
+			},
+			_ => {
+				println!("Empty command")
+			},
+		}
+	}
+}
+
+impl Server {
 	pub fn new(adress: &str, port: &str) -> Self {
 		Server {
 			listener: TcpListener::bind(format!("{}:{}", adress, port)).expect("Failed to listen localhost:8080"),
@@ -42,25 +132,24 @@ impl Server {
 		true
 	}
 	
-	fn read(mut stream: &TcpStream) -> String {
+	fn read(stream: &TcpStream) -> String {
 		let mut message = String::new();
 		let mut reader = BufReader::new(stream);
-		if let Ok(size) = reader.read_line(&mut message) {
+		if let Ok(_) = reader.read_line(&mut message) {
 			println!(" <<== {}", message);
 			return message;
 		}
 		println!(" XX== Failed to read response");
-		"".to_string()
+		String::new()
 	}
 	
-	fn new_client(mut stream: &TcpStream) {
-		if !Server::write(stream, "Hello") { return }
-		Server::read(stream);
+	fn new_client(stream: &TcpStream) {
+		if !Server::write(stream, "Hello") { return; }
+		Command::from_string(&Server::read(stream)).exec();
 		stream.shutdown(Shutdown::Both).expect("Failed to close connection");
 	}
 	
 	pub fn run(&self) {
-		
 		println!("Waiting for client");
 		for client in self.listener.incoming() {
 			match client {
